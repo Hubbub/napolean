7bd29969bb60fdd96ebdc414282f126dd1d8bbbd
Jon Wood <jon@blankpad.net>
2012-11-26 13:34:51 +0000

    Deal with dodgy time formats from mpstat

--- lib/collectors/cpu_load.rb
+++ lib/collectors/cpu_load.rb
@@ -3,7 +3,9 @@ if $? == 0
   output = `mpstat 1 1` # Get stats on a single 1 second sample.
   fields = output.split("\n")[3].split(/\s+/)
 
-  i = 2
+  # Some versions of mpstat output time as 01:35:40 PM, while others us a 24 hours clock.
+  i = (fields[2] == "all") ? 3 : 2
+
   %w(user nice sys iowait irq soft steal guest idle).each do |field|
     queue_gauge("system.cpu.#{field}", fields[i])
     i += 1
