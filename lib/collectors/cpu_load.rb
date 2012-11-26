02585105cf7fc03780810a00554067785138346c
Jon Wood <jon@blankpad.net>
2012-11-26 14:06:19 +0000

    Track total CPU usage

--- lib/collectors/cpu_load.rb
+++ lib/collectors/cpu_load.rb
@@ -6,10 +6,16 @@ if $? == 0
   # Some versions of mpstat output time as 01:35:40 PM, while others us a 24 hours clock.
   i = (fields[2] == "all") ? 3 : 2
 
-  %w(user nice sys iowait irq soft steal guest idle).each do |field|
+  sum = 0
+  %w(user nice sys iowait irq soft steal guest).each do |field|
     queue_gauge("system.cpu.#{field}", fields[i])
+    sum += fields[i].to_f
+
     i += 1
   end
+
+  queue_gauge("system.cpu.idle", fields[i])
+  queue_gauge("system.cpu.total", sum)
 else
   $log.warn("Skipping CPU load because mpstat is not available. Install the sysstat package.")
 end
